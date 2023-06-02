f5d9b72984467fbce2443e5f8d654ec1a3a0e309
Toni Tauro <toni.tauro@adfinis.com>
2023-06-02 09:15:51 +0200

    feat(cronjob/sa/script): suggestions from @lukasgr
    Signed-off-by: Toni Tauro <toni.tauro@adfinis.com>

--- kubernetes/vault-snapshot.sh
+++ kubernetes/vault-snapshot.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-# create tmpdir
-
-mkdir -p /tmp/vault-snapshots
-
 # authenticate using kubernetes auth
 export JWT=$(cat /var/run/secrets/kubernetes/io/serviceaccount/token)
 export VAULT_TOKEN=$(vault write -field=token  auth/kubernetes/login role=$VAULT_ROLE jwt=$JWT)
@@ -13,7 +9,7 @@ export VAULT_ADDR=$(vault status -format=yaml | egrep -o '^leader_addr.*' | awk 
 
 # create snapshot
 
-vault operator raft snapshot save /tmp/vault-snapshots/vault_$(date +%F-%H%M).snapshot
+vault operator raft snapshot save /vault-snapshots/vault_$(date +%F-%H%M).snapshot
 
 # upload to s3
-s3cmd put /tmp/vault-snapshots/* $S3_URI --host=$S3_HOST --host-bucket=$S3_BUCKET
+s3cmd put /vault-snapshots/* $S3_URI --host=$S3_HOST --host-bucket=$S3_BUCKET
