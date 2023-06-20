68e85131ad25673d081c595f709bd9f74f7879a2
Toni Tauro <toni.tauro@adfinis.com>
2023-06-20 15:02:36 +0200

    fix(entrypoint.sh): shellcheck
    Signed-off-by: Toni Tauro <toni.tauro@adfinis.com>

--- kubernetes/entrypoint.sh
+++ kubernetes/entrypoint.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env sh
 
 # authenticate using kubernetes auth
-export JWT=$(cat /var/run/secrets/kubernetes/io/serviceaccount/token)
-export VAULT_TOKEN=$(vault write -field=token  auth/kubernetes/login role=$VAULT_ROLE jwt=$JWT)
+JWT=$(cat /var/run/secrets/kubernetes/io/serviceaccount/token)
+export JWT
+VAULT_TOKEN=$(vault write -field=token  auth/kubernetes/login role="${VAULT_ROLE}" jwt="${JWT}")
+export VAULT_TOKEN
 
 # use the leader node as VAULT_ADDR
-export VAULT_ADDR=$(vault status -format=yaml | egrep -o '^leader_addr.*' | awk '{print $2}')
+VAULT_ADDR=$(vault status -format=yaml | grep -Eo '^leader_addr.*' | awk '{print $2}')
+export VAULT_ADDR
 
 # create snapshot
 
-vault operator raft snapshot save /vault-snapshots/vault_$(date +%F-%H%M).snapshot
+vault operator raft snapshot save /vault-snapshots/vault_"$(date +%F-%H%M)".snapshot
 
 # upload to s3
-s3cmd put /vault-snapshots/* $S3_URI --host=$S3_HOST --host-bucket=$S3_BUCKET
+s3cmd put /vault-snapshots/* "${S3_URI}" --host="${S3_HOST}" --host-bucket="${S3_BUCKET}"
