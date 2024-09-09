44d1ca453c9e4264c06468355a09075940bb32e5
Andreas Gruhler <andreas.gruhler@adfinis.com>
2024-09-09 10:43:21 +0200

    fix: shellcheck

--- kubernetes/vault-snapshot.sh
+++ kubernetes/vault-snapshot.sh
@@ -15,12 +15,12 @@ s3cmd put /vault-snapshots/* "${S3_URI}" --host="${S3_HOST}" --host-bucket="${S3
 # remove expired snapshots
 if [ "${S3_EXPIRE_DAYS}" ]; then
     s3cmd ls "${S3_URI}" --host="${S3_HOST}" --host-bucket="${S3_BUCKET}" | while read -r line; do
-        createDate=$(echo $line | awk {'print $1" "$2'})
+        createDate=$(echo "$line" | awk '{print $1" "$2}')
         createDate=$(date -d"$createDate" +%s)
         olderThan=$(date --date "${S3_EXPIRE_DAYS} days ago" +%s)
-        if [[ $createDate -lt $olderThan ]]; then
-            fileName=$(echo $line | awk {'print $4'})
-            if [[ $fileName != "" ]]; then
+        if [ $createDate -lt $olderThan ]; then
+            fileName=$(echo "$line" | awk '{print $4}')
+            if [ $fileName != "" ]; then
                 s3cmd del "${S3_URI}/$fileName" --host="${S3_HOST}" --host-bucket="${S3_BUCKET}"
             fi
         fi
