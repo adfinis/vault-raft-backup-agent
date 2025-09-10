5eee3738e786d47c38ebb05858315b93e934c259
Yannis Rachdi <yannis.rachdi@adfinis.com>
2025-09-10 14:19:26 +0200

    docs: missing comments
    Signed-off-by: Yannis Rachdi <yannis.rachdi@adfinis.com>

--- kubernetes/bao-snapshot.sh
+++ kubernetes/bao-snapshot.sh
@@ -10,40 +10,16 @@ echo "Using Vault auth path: $VAULT_AUTH_PATH"
 # Authenticate with Vault
 JWT=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 export JWT
-<<<<<<< HEAD:kubernetes/vault-snapshot.sh
 
-echo "Using Vault auth path: $VAULT_AUTH_PATH"
-VAULT_TOKEN=$(vault write -field=token "auth/$VAULT_AUTH_PATH/login" role="${VAULT_ROLE}" jwt="$JWT")
-export VAULT_TOKEN
-||||||| parent of 2d9c663 (feat: OpenBao raft backup agent):kubernetes/vault-snapshot.sh
-VAULT_TOKEN=$(vault write -field=token  auth/kubernetes/login role="${VAULT_ROLE}" jwt="${JWT}")
-export VAULT_TOKEN
-=======
+echo "Using OpenBao auth path: $BAO_AUTH_PATH"
 BAO_TOKEN=$(bao write -field=token  auth/${BAO_AUTH_PATH}/login role="${BAO_ROLE}" jwt="${JWT}")
 export BAO_TOKEN
->>>>>>> 2d9c663 (feat: OpenBao raft backup agent):kubernetes/bao-snapshot.sh
 
-<<<<<<< HEAD:kubernetes/vault-snapshot.sh
 # Create snapshot
-vault operator raft snapshot save /vault-snapshots/vault_"$(date +%F-%H%M)".snapshot
-||||||| parent of 2d9c663 (feat: OpenBao raft backup agent):kubernetes/vault-snapshot.sh
-# create snapshot
-vault operator raft snapshot save /vault-snapshots/vault_"$(date +%F-%H%M)".snapshot
-=======
-# create snapshot
 bao operator raft snapshot save /bao-snapshots/bao_"$(date +%F-%H%M)".snapshot
->>>>>>> 2d9c663 (feat: OpenBao raft backup agent):kubernetes/bao-snapshot.sh
 
-<<<<<<< HEAD:kubernetes/vault-snapshot.sh
 # Upload to S3
-s3cmd put /vault-snapshots/* "${S3_URI}" --host="${S3_HOST}" --host-bucket="${S3_BUCKET}"
-||||||| parent of 2d9c663 (feat: OpenBao raft backup agent):kubernetes/vault-snapshot.sh
-# upload to s3
-s3cmd put /vault-snapshots/* "${S3_URI}" --host="${S3_HOST}" --host-bucket="${S3_BUCKET}"
-=======
-# upload to s3
 s3cmd put /bao-snapshots/* "${S3_URI}" --host="${S3_HOST}" --host-bucket="${S3_BUCKET}"
->>>>>>> 2d9c663 (feat: OpenBao raft backup agent):kubernetes/bao-snapshot.sh
 
 # Remove expired snapshots
 if [ "${S3_EXPIRE_DAYS}" ]; then
